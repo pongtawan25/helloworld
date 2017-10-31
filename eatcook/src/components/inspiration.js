5a4ecdb9666d9345981355f23287d03b17a5b289
chynurt <chynury@icloud.com>
2017-10-31 19:12:46 +0700

    updated

--- eatcook/src/components/inspiration.js
+++ eatcook/src/components/inspiration.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import Navbar from './navbar'
+import './../static/css/style.css'
 
 
 const Inspiration = () => (
     <div>
         <Navbar />
-        <div className="row text-center">
-            <div className="col-md-3">
-
-            </div>
+        <div className="bginspiration">
+        
+        <div className="row texthadeleft ">
+                <div className="col-md-6 ">
+                    <div className="text-center">
+                        <h1> INSPIRATION  <br /> </h1>
+                    <p>uiuuuuuuuuuyyyjwdefrgthyjuidefrgthyjuikerfgthyjuikerfgt
+                        hyjurfgthyyjwdefrgthyjuidefrgthyjuikerfgthyjuikerfgt
+                        hyjurfgthyujyjwdefrgthyjuidefrgthyjuikerfgthyjuikerfgt
+                        hyjurfgthyujuj</p>
+                    </div>
+                </div>
+        </div>       
+        
         </div>
+
+ 
     </div>
 )
 
