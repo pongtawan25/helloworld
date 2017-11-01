75522cc9523aab4552b337e7c29cd936c51d2174
chynurt <chynury@icloud.com>
2017-11-01 12:34:59 +0700

    นี่คือพุชไปปะ งง อะ ทำต่อที

--- eatcook/src/components/inspiration.js
+++ eatcook/src/components/inspiration.js
@@ -6,18 +6,22 @@ import './../static/css/style.css'
 const Inspiration = () => (
     <div>
         <Navbar />
-        <div className="bginspiration">
         
-        <div className="row texthadeleft ">
-                <div className="col-md-6 ">
+        <div className="row bginspiration">
+                <div className="offset-md-1 col-md-4">
                     <div className="text-center">
-                        <h1> INSPIRATION  <br /> </h1>
-                    <p> การประกอบอาหาร</p>
+                        <h1 className="inshead"> INSPIRATION <br /> </h1>
                     </div>
+
+                        <p className="test">
+                        การประกอบอาหารนั้นนอกจะมีไว้เพื่อรับประทานแล้ว <br/>ยังทำเป็นอาชีพ หรืองานอดิเรกได้ ในสมัยนี้มีอาหารมากมายหลายเมนู เมนูที่มีแต่เดิมแล้วนำมาประยุกต์ใหม่ก็มี หรืออาจจะคิดเมนูใหม่ที่ไม่เคยมีมาก่อนเลยก็มี จึงอยากจะเชิญชวนคนที่รักการทำอาหาร หรือชื่นชอบในการรับประทานอาหารมาร่วมกันแชร์เมนูอาหารและประกอบอาหารเพื่อแลกเปลี่ยนความรู้ต่อๆกัน
+                        </p>
+                    
                 </div>
-        </div>       
-        
         </div>
+
+
+        
 
  
     </div>
