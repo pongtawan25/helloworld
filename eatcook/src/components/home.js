c2de8d5c70e17f748f0195d0d5ce5af6c1cfbd6b
Kanisorn Chaiwichachan <khanysorn@outlook.com>
2017-11-02 00:24:16 +0700

    เพิ่ม Effect Navbar ให้สวยขึ้น เพิ่มหน้า Login แล้ว เดี๋ยวแก้นิดหน่อย

--- eatcook/src/components/home.js
+++ eatcook/src/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import pichome from './../static/img/crossed-knife-and-fork.png'
 import Navbar from './navbar'
+import './../static/css/animate.css'
 
 const Home = () => (
 
@@ -8,10 +9,10 @@ const Home = () => (
     <Navbar />
     <div className="row bghome">
       <div className="text-center">
-        <h4 className="textwel">Welcome to</h4>
-        <h1 className="textcook">COOK AND EAT</h1>
-        <p className="textlet">LET'S COOKING AND SHARE YOUR IDEA</p>
-        <img src={pichome} width="50px" />
+        <h4 className="textwel animated fadeInUp">Welcome to</h4>
+        <h1 className="textcook animated fadeInUp">COOK AND EAT</h1>
+        <p className="textlet animated fadeInUp">LET'S COOKING AND SHARE YOUR IDEA</p>
+        <img className="animated fadeInUp"src={pichome} width="50px" />
       </div>
     </div>
   </div>
